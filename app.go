0391150e2d7ffe32b2e19b76d1f1d75db80659d3
Cleberson <pauluci.cleberson@gmail.com>
2024-02-10 19:20:52 -0300

    Create functions to support the code

--- app.go
+++ app.go
@@ -3,30 +3,49 @@ package main
 import "fmt"
 
 func main() {
-	var revenue float64
-	var expenses float64
-	var taxRate float64
+	//var revenue float64
+	//var expenses float64
+	//var taxRate float64
 
 	// get revenue
-	fmt.Print("Revenue: ")
-	fmt.Scan(&revenue)
+	//fmt.Print("Revenue: ")
+	//fmt.Scan(&revenue)
+	revenue := getInputValue("Revenue: ")
 
 	// get expenses
-	fmt.Print("Expenses: ")
-	fmt.Scan(&expenses)
+	//fmt.Print("Expenses: ")
+	//fmt.Scan(&expenses)
+	expenses := getInputValue("Expenses: ")
 
 	// get taxRate
-	fmt.Print("Tax Rate: ")
-	fmt.Scan(&taxRate)
+	//fmt.Print("Tax Rate: ")
+	//fmt.Scan(&taxRate)
+	taxRate := getInputValue("Tax Rate: ")
 
 	// earnings before tax
-	ebt := revenue - expenses
+	//ebt := revenue - expenses
 
 	// earnings after tax
-	profit := ebt * (1 - taxRate/100)
+	//profit := ebt * (1 - taxRate/100)
 
 	// ratio EBT / profit
-	ratio := ebt / profit
+	//ratio := ebt / profit
+
+	ebt, profit, ratio := calculateEbtProfitAndRatio(revenue, expenses, taxRate)
 
 	fmt.Printf("EBT: %.2f\nProfit: %.2f\nRatio: %.2f\n", ebt, profit, ratio)
+}
+
+func getInputValue(text string) float64 {
+	fmt.Print(text)
+	var value float64
+	fmt.Scan(&value)
+	return value
+}
+
+func calculateEbtProfitAndRatio(revenue, expanses, taxRate float64) (ebt float64, profit float64, ratio float64) {
+	ebt = revenue - expanses
+	profit = ebt * (1 - taxRate/100)
+	ratio = ebt / profit
+	return
 }
